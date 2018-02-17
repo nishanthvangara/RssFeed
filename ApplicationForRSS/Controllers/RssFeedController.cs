2a5061d18f80ce38d48de09054b6400ff7b1d07a
Nishanth Vangara <nishanthvangara@gmail.com>
2018-02-18 01:53:12 +0530

    Check in Project

--- ApplicationForRSS/Controllers/RssFeedController.cs
+++ ApplicationForRSS/Controllers/RssFeedController.cs
@@ -138,33 +138,9 @@ namespace ApplicationForRSS.Controllers
                 }
                 else
                 {
-                    var r = Serializer.Deserialize<MyClass>(res.Content.ToString());
+                    
                 }
             return Ok();
-        }
-
-        public static class Serializer
-        {
-            public static T Deserialize<T>(string input) where T : class
-            {
-                System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
-
-                using (StringReader sr = new StringReader(input))
-                {
-                    return (T)ser.Deserialize(sr);
-                }
-            }
-
-            public static string Serialize<T>(T ObjectToSerialize)
-            {
-                XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
-
-                using (StringWriter textWriter = new StringWriter())
-                {
-                    xmlSerializer.Serialize(textWriter, ObjectToSerialize);
-                    return textWriter.ToString();
-                }
-            }
-        }
+        }        
     }
 }
